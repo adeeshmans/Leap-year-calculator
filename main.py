b15cd2c983e4528c5004170010e2e72820a02f9d
Adeesh M Shrestha <35621337+adeeshmans@users.noreply.github.com>
2023-04-02 20:02:21 -0500

    comments
    comments

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-# Leap year calculator
+# Leap Year Calculator
+# checks if the year is leap year or not
 year = int(input("Which year do you want to check? "))
 
 # Formula
 if year % 4 == 0:
-  if year % 100 == 0:
-    if year % 400 == 0:
-      print("Leap year")
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year")
+        else:
+            print("Not leap year.")
     else:
-      print("Not leap year.")
-  else:
-    print("Leap year.")
+        print("Leap year.")
 else:
-  print("Not leap year.")
+    print("Not leap year.")
